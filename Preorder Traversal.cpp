dbdf993b73ab2d9bf633a5c1776a32f7d9eb5ca4
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-06-04 23:08:40 +0530

    Create Preorder Traversal.cpp

---
+++ Preorder Traversal.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+vector<int> getPreOrderTraversal(TreeNode *root)
+{
+   vector<int> ans;
+   if(root==NULL) return ans;
+   stack<TreeNode *> s;
+   s.push(root);
+   while(!s.empty()){
+       root=s.top();
+       s.pop();
+       ans.push_back(root->data);
+       if(root->right!=NULL) s.push(root->right);
+       if(root->left!=NULL) s.push(root->left);
+   }
+   return ans;
+}
