a6ff93c56d96ca0c963cdd77941fb5f47451c7c6
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-06-04 19:07:50 +0530

    Create Merge Intervals.cpp

---
+++ Merge Intervals.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+vector<vector<int>> mergeIntervals(vector<vector<int>> &intervals)
+{
+    sort(intervals.begin() , intervals.end());
+    vector<vector<int>>ans;
+    int start=intervals[0][0];
+    int end=intervals[0][1];
+    for(int i=1 ; i<intervals.size() ; i++)
+    {
+        if(intervals[i][0]>end)
+        {
+            ans.push_back({start , end});
+            start=intervals[i][0];
+            end=intervals[i][1];
+        }
+        else if(end<=intervals[i][1])
+        {
+            end=max(end , intervals[i][1]);
+        }
+    }
+    ans.push_back({start , end});
+    return ans;
+}
+// tc : (nlogn) sc: o(1);
