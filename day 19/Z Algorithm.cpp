7c1761d9d94738170c9eadcb31bff2d2b32f99ae
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-06-23 20:20:12 +0530

    Create Z Algorithm.cpp

---
+++ day 19/Z Algorithm.cpp
@@ -0,0 +1,13 @@
+int zAlgorithm(string s, string p, int n, int m)
+{
+	// Write your code here
+	int count = 0;
+    for(int i=0;i<n;i++){
+        if(s[i]==p[0]){
+            if(s.substr(i,m)==p){
+                count++;
+            }
+        }
+    }
+    return count;
+}
