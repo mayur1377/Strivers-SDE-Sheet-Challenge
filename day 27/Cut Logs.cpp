593b7a85737047afef1dcdfe2b8ab8d53b1a0a6d
MAYUR <74107137+mayur1377@users.noreply.github.com>
2023-07-02 16:09:29 +0530

    Update Cut Logs.cpp

--- day 27/Cut Logs.cpp
+++ day 27/Cut Logs.cpp
@@ -1,14 +1,31 @@
-int f(int k, int start, int end)
+#include<bits/stdc++.h>
+int dp[10005][105];
+int Solve(int n,int e)
 {
-    if(end == start)
-        return 0;
-    if(k == 1)
-        return (end-start);
-    int mid = (start + end + 1)/2;
-    return 1 + max(f(k-1, start, mid-1), f(k, mid, end));
+   if(e==1)
+     return n;
+    if(n==1 or n==0)
+         return n;
+    if(dp[n][e]!=-1)
+     return dp[n][e];
+    int ans=INT_MAX;
+    int low=1,high=n;
+   while(low<=high)
+   {
+        int mid=(low+high)/2;
+        int left=Solve(mid-1,e-1);
+        int right=Solve(n-mid,e);
+        int temp=1+max(left,right);
+         if(left>right)
+            high=mid-1; 
+       else 
+            low=mid+1;
+        ans=min(ans,temp);
+    }
+    return dp[n][e]=ans;
 }
 int cutLogs(int k, int n)
 {
-    return f(k, 0, n);
+   memset(dp,-1,sizeof(dp));
+   return Solve(n,k);
 }
- 
